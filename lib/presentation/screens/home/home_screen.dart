c8de1355de51027dcbab7d2fd6969cfd98b01824
Manuel Walter Navarro Z <manuel08n@gmail.com>
2023-07-31 08:18:30 -0500

    home_screen implement

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_item.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,35 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter + Material 3'),
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ListView.builder(
+      physics: BouncingScrollPhysics(),
+      itemCount: appMenuItems.length,
+      itemBuilder: (BuildContext context, int index) {
+        final menuItem = appMenuItems[index];
+        return ListTile(
+          leading: Icon(
+            menuItem.icon,
+            color: colors.primary,
+          ),
+          title: Text(menuItem.title),
+          subtitle: Text(menuItem.subTitle),
+          trailing: Icon(Icons.arrow_forward_ios_rounded,color: colors.primary,),
+          onTap: (){
+            //TODO:navegar a otra pantalla
+          },
+        );
+      },
     );
   }
 }
