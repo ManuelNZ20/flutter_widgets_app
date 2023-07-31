0ce5e30556dd5e28cf75d10489741fe51522194d
Manuel Walter Navarro Z <manuel08n@gmail.com>
2023-07-31 09:45:23 -0500

    configuration routes go_router

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_item.dart';
 import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
@@ -40,7 +41,8 @@ class _HomeView extends StatelessWidget {
             // Navigator.of(context).push(
             //   MaterialPageRoute(builder: (context) => ButtonsScreen(),)
             // );
-            Navigator.pushNamed(context, menuItem.link);
+            // Navigator.pushNamed(context, menuItem.link);
+            context.push(menuItem.link);
           },
         );
       },
